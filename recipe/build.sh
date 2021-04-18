d7d9f6abf0b685fd81f918b76d3473b0b3aee6ce
Ista Zahn <izahn@hbs.edu>
2021-04-18 18:30:56 -0400

    build libqhull

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ CMAKE_GENERATOR="Unix Makefiles"
 
 cmake ${CMAKE_ARGS} -G "$CMAKE_GENERATOR" \
     -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} 
-
-make -j${CPU_COUNT}
+    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}
+make -j${CPU_COUNT} all libqhull ## deprecated, needed by octave
 make install
